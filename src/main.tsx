d1966550c7c7947c67201e1eac97a6ae9919fdbc
JoverZhang <joverzh@gmail.com>
2022-11-13 17:37:08 +0800

    chore: import dependency notistack

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './samples/node-api'
 import 'styles/index.css'
+import '@/common/advance'
+import { SnackbarProvider } from 'notistack'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <SnackbarProvider maxSnack={3} autoHideDuration={3000}>
+      <App />
+    </SnackbarProvider>
   </React.StrictMode>,
 )
 
